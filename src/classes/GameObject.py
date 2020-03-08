ca89f1467fef273f3ad2984a99daf772ac0196e6
Montanha <arieivovatsug@gmail.com>
2020-03-08 11:16:49 -0300

    updating gameobject

--- src/classes/GameObject.py
+++ src/classes/GameObject.py
@@ -1,16 +1,17 @@
 import pygame
 
-class GameObject:
-    def __init__ (self, position=(720, 480), width=0, height=0, velocity=0):
+class GameObject(pygame.Rect):
+    def __init__ (self, position = (720, 480), width = 100, height = 100, velocity = [0, 0]):
+        
+        pygame.Rect.__init__(self)
         self.velocity = velocity
-
         self.moving = False
         self.moveCount = 0
         self.attacking = False
         self.attackCount = 0
 
 
-        self.rect = pygame.Rect(position[0], position[1], width, height)
+
 
 
 
